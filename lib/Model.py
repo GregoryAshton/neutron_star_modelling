83fe0309989fc3043e0943ceb20f3f3bcb943136
Gregory Ashton <ga7g08@soton.ac.uk>
2013-05-07 14:48:57 +0100

    Run_Cython for t1 now, need to add eta and then okay it for all runs

--- lib/Model.py
+++ lib/Model.py
@@ -115,23 +115,38 @@ def Run(Input_Dictionary):
 	return file_name
 
 def Run_Cython(Input_Dictionary):
+	""" Create a generic write file, compile and run in C. 
+
+	Keyword arguments must be passed as a dictionary
+	chi:float [degrees] -- angle between magnetic dipole and z axis
+	epsI:float []-- elastic deformation 
+	epsA :float []-- magnetic deformation 
+	omega0 :float [Hz]-- initial spin period 
+	either 
+		eta :float []-- Threshold for which to stop the simulation. Simulation is initiated using omega**2.0 < eta*omega_0**2.0 
+	or
+		t1:float [s] -- Time for which to run simulation for		
+
+	Optional arguments
+	err : float[] -- Error argument to pass to the GCC compiler 
+
+	For help with the GCC compiler see documentation at http://www.gnu.org/software/gsl/manual/html_node/Ordinary-Differential-Equations.html 
+	The generic script is written by Write_File in File_Functions
 	"""
 
-	"""
-
-	from scipy.integrate import ode
-	from pylab import cos,sin,pi
-
+	import nsmod_cython
 
 	#  Required paramaters
 
 	# Check if the anomalous torque should be used or not and initiate the file_name
 	file_name_list = []
+
+	# We ask the user to turn of the anom torque, it is by default, on, in the program. 
 	if Input_Dictionary.get('no_anom'):
-		file_name_list .append("no_anom")
-		no_anom=True
+		file_name_list .append("no_anom_")
+		anom_torque=False
 	else :
-		no_anom=False
+		anom_torque=True
 		
 	# At the moment these erros do not raise correctly. 
 
@@ -165,161 +180,38 @@ def Run_Cython(Input_Dictionary):
 
 	# Take either t1 or eta but not both 
 	# Note: For now we will use only t1 
-	try :
-		eta = str(Input_Dictionary['eta'])
-		file_name_list.append("_eta_"+str(Input_Dictionary['eta']))
-		eta_relative = str(float(eta)*pow(float(omega0),2)) 
-	except KeyError:
-		eta = None
+#	try :
+#		eta = str(Input_Dictionary['eta'])
+#		file_name_list.append("_eta_"+str(Input_Dictionary['eta']))
+#		eta_relative = str(float(eta)*pow(float(omega0),2)) 
+#	except KeyError:
+#		eta = None
 	try :
 		t1 = str(Input_Dictionary['t1'])
 		file_name_list.append("_t1_"+str(Input_Dictionary['t1']))
 	except KeyError:
-		t1 = None
+		print "ERROR: You must specify t1, eta is not yet implemented "
+		return
 
 #	#  Additional Arguments
 	try :
-		err = str(Input_Dictionary['err'])
+		error = str(Input_Dictionary['error'])
 	except KeyError:
 		# Use a default value 
-		err = 1e-12
+		error = 1e-12
 
-	#------------------------------------
+	# Create file name 
+	file_name_list.append(".txt")
+	file_name = "".join(file_name_list)
 
-	import scipy.weave as weave
+	nsmod_cython.main(chi_degrees=float(chi_degrees) , file_name = file_name,
+						 epsA = float(epsA) , epsI=float(epsI) , 
+						omega0=float(omega0) , t1 = float(t1) ,
+						anom_torque = anom_torque , error=float(error))
 
-	def run(chi_degrees,epsI,epsA,omega0,t1,err):
+	
 
-		assert(type(chi_degrees) == float)
-		assert(type(epsI) == float)
-		assert(type(epsA) == float)
-		assert(type(omega0) == float)
-		assert(type(t1) == float)
-		assert(type(err) == float)
-		
-		code = '''
-/********************************************************************/
-/*                                                                  */
-/* Evolution of omega , a and phi for ODEs from Goldreich           */
-/*                                                                  */
-/********************************************************************/
-
-
-/* Function containing the ODEs*/
-
-int
-func (double t, const double y[], double f[] , void *params)
-{
-	/* Import the three time dependant variables from y[]*/
-	double wx=y[0]; 
-	double wy=y[1];
-	double wz=y[2];
-
-	/* Calculate w*w labelled as w_2 */
-	double w_2 = pow(wx,2.0)+pow(wy,2.0)+pow(wz,2.0);
-
-	/* Import the constant variables from params*/
-	double *P = (double *) params;
-	double lambda = P[0];    /* = 2R/3C */
-	double eps_I= P[1];
-	double eps_A = P[2];
-	double chi = P[3];      
-
-	/* Calculate the angular parts of the three equations to avoid repeating the same calculation */
-	double Cx = cos(chi) ;
-	double Sx = sin(chi) ;
-
-	/* Define the three ODEs in f[] as functions of the above variables*/
-	f[0] = eps_A*(lambda*w_2*Cx*(wz*Sx-wx*Cx)+(Sx*wx+Cx*wz)*wy*Cx) - wy*wz*eps_I;
-
-	f[1] = eps_A*(-lambda*w_2*wy+(Sx*wx+Cx*wz)*(wz*Sx-wx*Cx)) + wx*wz*eps_I;
-
-	f[2] = (eps_A/(1+eps_I)) * (lambda*w_2*Sx*(wx*Cx - wz*Sx) -(Sx*wx+Cx*wz)*wy*Sx) ;
-
-	return GSL_SUCCESS;
-}
-
-/* Currently jac is unused by the ODE solver so is left empty*/
-int
-jac (double t, const double y[], double *dfdy, 
-	 double dfdt[], void *params)
-{
-
-	return GSL_SUCCESS;
-}
-
-int
-main (void)
-{
-	/* Input parameters*/
-	double chi=M_PI*chi_degrees/180; 		// Radians	
-	double R = 1.0e6;               	// cm
-	double c_speed = 3e10 ;         	// cm/s
-	double lambda = 2*R / (3*c_speed) ;     // s
-
-	double eps_I ;           
-	double eps_A ;
-
-	/* Initial value of omega, a_int is the initial angle against the z-axis*/    
-	double omega_0; 
-	double a_int=M_PI*50.0/180; 
-	double y[3] = { omega_0*sin(a_int), 0.0 , omega_0*cos(a_int) };
-
-	// Start and stop times for integration:
-	double t = 0.0, t1 ;
-
-	double params[4];	
-	params[0] = lambda;
-	params[1] = eps_I ; 
-	params[2] = eps_A;
-	params[3] = chi;
-
-
-	/*************************************************************/
-	/* GSL ODE desclarations                                     */
-	/*************************************************************/
-
-	// Choose the stepping alogrithm:
-	const gsl_odeiv2_step_type * T  = gsl_odeiv2_step_rk8pd;
-
-	// Specify number of dependent variables:
-	gsl_odeiv2_step * s = gsl_odeiv2_step_alloc (T, 3);
-
-	// Specify absolute and relative errors (actual allowed error is a linear combination of these):
-	gsl_odeiv2_control * c = gsl_odeiv2_control_y_new (err, err);
-
-	// Created evolution function:
-	gsl_odeiv2_evolve * e  = gsl_odeiv2_evolve_alloc (3);
-
-	// Tell GSL the dimension of the system and the names of the functions containing the ODEs:
-	gsl_odeiv2_system sys = {func, jac, 3, &params};
-
-	// Trial step size for first step
-	double h = 1e-10;
-
-	/*************************************************************/
-
-	while (t < t1)
-	{
-		int status = gsl_odeiv2_evolve_apply (e, c, s, &sys, &t, t1, &h, y);
-		if (status != GSL_SUCCESS)
-	 	{
-			printf ("error, return value=%d\n", status);
-			break;
-	 	}
-
-		printf ("%.16e %.16e %.16e %.16e\n", t, y[0], y[1] , y[2]);
-	}
-
-	   gsl_odeiv2_evolve_free (e);
-	   gsl_odeiv2_control_free (c);
-	   gsl_odeiv2_step_free (s);
-	return 0;
-}
-		''' 
-		return weave.inline(code,['chi_degrees','epsI','epsA','omega0','t1','err'], compiler = 'gcc',headers = ['<stdio.h>','<gsl/gsl_errno.h>','<gsl/gsl_matrix.h>','<gsl/gsl_odeiv2.h>','<math.h>'])
-
-	run(float(chi_degrees),float(epsI),float(epsA),float(omega0),float(t1),float(err))
+	
 	
 
 
---
+++ src/nsmod_cython.pyx
@@ -0,0 +1,144 @@
+from cython_gsl cimport *
+import math
+
+# Functions to solve
+cdef int with_anom_torque (double t, double y[], double f[], void *params) nogil:
+	""" Function defining the ODEs with the anomalous torque """
+	# Define the variables used in the calculation
+	cdef double wx,wy,wz,w_2,epsI,epsA,chi,Lambda
+
+	# Import the three time dependant variables from y[] 
+	wx=y[0]
+	wy=y[1]
+	wz=y[2]
+	w_2 = pow(wx,2)+pow(wy,2)+pow(wz,2)
+
+	# Import the constant variables from params
+	Lambda = (<double *> params)[0]    #= 2R/3C */
+	epsI= (<double *> params)[1]
+	epsA = (<double *> params)[2]
+	chi = (<double *> params)[3] 
+
+	# Calculate the angular parts of the three equations to avoid repeated calculation
+	cdef double Cx,Sx
+	Cx = cos(chi) 
+	Sx = sin(chi) 
+
+	#  Define the three ODEs in f[] as functions of the above variables
+	f[0] = epsA*(Lambda*w_2*Cx*(wz*Sx-wx*Cx)+(Sx*wx+Cx*wz)*wy*Cx) - wy*wz*epsI;
+
+	f[1] = epsA*(-Lambda*w_2*wy+(Sx*wx+Cx*wz)*(wz*Sx-wx*Cx)) + wx*wz*epsI;
+
+	f[2] = epsA*pow(1+epsI,-1) * (Lambda*w_2*Sx*(wx*Cx - wz*Sx) -(Sx*wx+Cx*wz)*wy*Sx) ;
+	return GSL_SUCCESS
+
+
+
+cdef int no_anom_torque (double t, double y[], double f[], void *params) nogil:
+	""" Function defining the ODEs with the anomalous torque """
+	# Define the variables used in the calculation
+	cdef double wx,wy,wz,w_2,epsI,epsA,chi,Lambda
+
+	# Import the three time dependant variables from y[] 
+	wx=y[0]
+	wy=y[1]
+	wz=y[2]
+	w_2 = pow(wx,2)+pow(wy,2)+pow(wz,2)
+
+	# Import the constant variables from params
+	Lambda = (<double *> params)[0]    #= 2R/3C */
+	epsI= (<double *> params)[1]
+	epsA = (<double *> params)[2]
+	chi = (<double *> params)[3] 
+
+	# Calculate the angular parts of the three equations to avoid repeated calculation
+	cdef double Cx,Sx
+	Cx = cos(chi) 
+	Sx = sin(chi) 
+
+	#  Define the three ODEs in f[] as functions of the above variables
+	f[0] = epsA*(Lambda*w_2*Cx*(wz*Sx-wx*Cx)) - wy*wz*epsI
+
+	f[1] = epsA*(-Lambda*w_2*wy) + wx*wz*epsI
+
+	f[2] = epsA*pow(1+epsI,-1) * (Lambda*w_2*Sx*(wx*Cx - wz*Sx)) 
+	return GSL_SUCCESS
+
+# Currently jac is unused by the ODE solver so is left empty
+cdef int jac (double t, double y[], double *dfdy, double dfdt[], void *params) nogil:
+
+	return GSL_SUCCESS
+
+
+
+def main (epsI=1.0e-6, epsA=1.0e-8 , omega0=1.0e4, error=1e-12, t1=1.0e8 , chi_degrees = 30.0 ,anom_torque=True,file_name = "generic.txt"):
+	"""  """
+
+	# Define default variables
+	cdef double chi,R,c_speed,Lambda
+
+	chi=math.pi*chi_degrees/180.0
+	R = 1.0e6            
+	c_speed = 3e10          
+	Lambda = 2*R / (3*c_speed) 
+	a_int=math.pi*50.0/180; 
+
+	# Pass them to params list
+	cdef double params[4]
+	params[0] = Lambda
+	params[1] = epsI
+	params[2] = epsA
+	params[3] = chi
+
+	# Setup the solver
+	cdef gsl_odeiv_step_type * T
+	T = gsl_odeiv_step_rk8pd
+
+	cdef gsl_odeiv_step * s
+	s = gsl_odeiv_step_alloc (T, 3)
+	cdef gsl_odeiv_control * c
+	c = gsl_odeiv_control_y_new (error, error)
+	cdef gsl_odeiv_evolve * e
+	e = gsl_odeiv_evolve_alloc (3)
+
+	cdef gsl_odeiv_system sys
+
+	# Test if the anomalous torque is required or not
+	if anom_torque :
+		sys.function = with_anom_torque 
+	else :
+		sys.function = no_anom_torque
+
+	sys.jacobian = jac
+	sys.dimension = 3
+	sys.params = params
+
+
+	cdef int i
+	cdef double t, y[3] ,h
+	h = 1e-15   # Initial step size
+	t = 0.0
+	y[0] = omega0*sin(a_int)
+	y[1] = 0.0
+	y[2] = omega0*cos(a_int)
+
+
+	cdef int status
+	# Currently we use python to write to file, would it be quicker to do this in C?
+	write_file = open(file_name,"w+")
+
+	while (t < t1):
+		status = gsl_odeiv_evolve_apply (e, c, s, &sys, &t, t1, &h, y)
+
+		if (status != GSL_SUCCESS):
+			break
+
+		write_file.write("%.16e %.16e %.16e %.16e\n" %(t, y[0], y[1],y[2]) )
+		
+		
+
+	gsl_odeiv_evolve_free (e)
+	gsl_odeiv_control_free (c)
+	gsl_odeiv_step_free (s)
+
+	write_file.close()

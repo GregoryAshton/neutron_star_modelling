68a09023922a562e486087091c863fe12301bf0e
Greg Ashton <gash789@gmail.com>
2014-08-31 22:06:32 +0100

    Updates illustrations

--- Illustrations/GWs/make_GW.py
+++ Illustrations/GWs/make_GW.py
@@ -3,56 +3,35 @@ import numpy as np
 from mpl_toolkits.mplot3d import axes3d
 import sys
 
+N = 500
+
+x = np.linspace(-15, 15, N)
+y = np.linspace(-15, 15, N)
+X, Y = np.meshgrid(x, y)
+R = np.sqrt(X ** 2 + Y **2)
+theta = np.arctan2(Y,X)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+R[R<5.6] = 0.0
+
 if "1" in sys.argv:
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    x = np.linspace(-15, 15, 500)
-    y = np.linspace(-15, 15, 500)
-    X, Y = np.meshgrid(x, y)
-
-    R = np.sqrt(X ** 2 + Y **2)
-    theta = np.arctan2(Y,X)
-
-    R[R<5.5] = 0
     R[Y<0] = 0
-
-    Z = np.sin(2*R) / (0.2*R) +  0.5*np.cos(3 * theta + 0.4)
-
-    ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
-    ax.set_zlim(-10, 10)
-
-    ax.set_xticks([])
-    ax.set_yticks([])
-    ax.set_zticks([])
-    plt.axis('off')
-    plt.savefig("GW1.pdf",bbox_inches='tight')
-
-    plt.show()
-
-if "2" in sys.argv:
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    x = np.linspace(-15, 15, 500)
-    y = np.linspace(-15, 15, 500)
-    X, Y = np.meshgrid(x, y)
-
-    R = np.sqrt(X ** 2 + Y **2)
-    theta = np.arctan2(Y,X)
-
-    R[R<5.5] = 0
+elif "2" in sys.argv:
     R[Y>0] = 0
 
-    Z = np.sin(2*R) / (0.2*R) +  0.5*np.cos(3 * theta + 0.4)
 
-    ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
-    ax.set_zlim(-10, 10)
+Z = np.sin(1.2*R) / (0.2*R) +  0.5*np.cos(3 * theta + 0.4)
 
-    ax.set_xticks([])
-    ax.set_yticks([])
-    ax.set_zticks([])
-    plt.axis('off')
+ax.plot_wireframe(X, Y, Z, rstride=40, cstride=40, 
+                  color="k")
+ax.set_zlim(-10, 10)
+
+plt.axis('off')
+
+if "1" in sys.argv:
+    plt.savefig("GW1.pdf",bbox_inches='tight', transparent=True)
+elif "2" in sys.argv:
     plt.savefig("GW2.pdf",bbox_inches='tight', transparent=True)
-
-    plt.show()
+plt.show()
--- Illustrations/signal_in_noise/make_illustration.py
+++ Illustrations/signal_in_noise/make_illustration.py
@@ -2,7 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy import pi
 
-N=1000
+#plt.xkcd()
+
+N=5000
+x = np.arange(N)
 
 rand = np.random.normal(0, 0.9, N)
 def phi(t, t0=0, f0=3, f1=-1e-1, f2=1e-2):
@@ -12,14 +15,22 @@ def phi(t, t0=0, f0=3, f1=-1e-1, f2=1e-2):
 time = np.linspace(0, 1, N)
 taylor = phi(time)
 
-fig, ax2 = plt.subplots(figsize=(8, 4))
-ax2.plot(np.random.normal(0, 1e-24, size=N), alpha=0.8, color="k")
-ax2.plot(1e-24 * np.sin(taylor), lw=3.0, color="r")
-ax2.set_xticks([])
-ax2.set_xlabel("Time", size=14)
-ax2.set_yticks([-1e-24, 0.0, 1e-24])
-ax2.set_ylim(-1.2e-24, 1.2e-24)
-ax2.set_ylabel("$h_{+}(t)$", rotation="horizontal", labelpad=20.5, size=20)
+signal = 1e-24 * np.sin(taylor)
+
+fig, ax = plt.subplots(figsize=(8, 4))
+noise_signal =  (signal + np.random.normal(0, 1e-24, size=N) 
+                 )  
+ax.plot(x, noise_signal, "o", alpha=0.8, markersize=3.0, color="k", label="Data")
+ax.plot(x, signal, lw=3.0, color="r", label="Template")
+ax.set_xticks([])
+ax.set_xlabel("Time", size=14)
+ax.set_yticks([-1e-24, 0.0, 1e-24])
+ax.set_ylim(-5.2e-24, 5.2e-24)
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+#ax.set_ylabel("$h_{+}(t)$", rotation="horizontal", labelpad=20.5, size=20)
+ax.set_ylabel("Strain", size=14); ax.set_yticks([])
 fig.tight_layout()
+ax.legend(loc=1, frameon=True)
 plt.savefig("CGW_example.pdf")
 plt.show()

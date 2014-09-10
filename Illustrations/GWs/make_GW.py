19b3cb4acd4367f948043769291949822f9056bd
Greg Ashton <gash789@gmail.com>
2014-09-10 08:59:08 +0100

    Changes GW illustrations

--- Illustrations/GWs/make_GW.py
+++ Illustrations/GWs/make_GW.py
@@ -3,7 +3,7 @@ import numpy as np
 from mpl_toolkits.mplot3d import axes3d
 import sys
 
-N = 500
+N = 200
 
 x = np.linspace(-15, 15, N)
 y = np.linspace(-15, 15, N)
@@ -17,21 +17,25 @@ ax = fig.add_subplot(111, projection='3d')
 R[R<5.6] = 0.0
 
 if "1" in sys.argv:
-    R[Y<0] = 0
+    R[Y<-1.4] = 0
 elif "2" in sys.argv:
-    R[Y>0] = 0
+    R[Y>=-1.4] = 0
 
 
 Z = np.sin(1.2*R) / (0.2*R) +  0.5*np.cos(3 * theta + 0.4)
 
-ax.plot_wireframe(X, Y, Z, rstride=40, cstride=40, 
-                  color="k")
+
+ax.plot_surface(X, Y, Z, rstride=4, cstride=4, 
+                  color="cyan", alpha=0.3)
+#ax.plot_wireframe(X, Y, Z, rstride=40, cstride=40, 
+                  #color="k")
+
 ax.set_zlim(-10, 10)
 
 plt.axis('off')
 
 if "1" in sys.argv:
-    plt.savefig("GW1.pdf",bbox_inches='tight', transparent=True)
+    plt.savefig("GW1.pdf",bbox_inches='tight', transparent=True, dpi=400)
 elif "2" in sys.argv:
-    plt.savefig("GW2.pdf",bbox_inches='tight', transparent=True)
+    plt.savefig("GW2.pdf",bbox_inches='tight', transparent=True, dpi=400)
 plt.show()

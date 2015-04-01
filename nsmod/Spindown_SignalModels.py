af27e46a0bf1dfd1eb8102a0396fe5c6bdec63cd
Gregory Ashton <gash789@gmail.com>
2015-04-01 16:09:32 +0100

    Adds full spin-down model with neglected terms

--- nsmod/Spindown_SignalModels.py
+++ nsmod/Spindown_SignalModels.py
@@ -34,3 +34,41 @@ def SignalModelWithGeometric(params, t, geometric=1):
                              .5*theta**2*(3+cos(2*chi))/(sin(chi))* sin(2*psi)
                              )
     return  (T1 + T2 + T3 + geometric * GEOMETRIC)/(2*pi)
+
+def SignalModelWithGeometricFull(params, t, geometric=0):
+    omega0, epsI, a0, chi, epsA = params
+
+    theta = a0
+
+    c = 3e10
+    R = 1e6
+    k = 2/3.0 * R/c * epsA
+    tauS = 1.0/(k * omega0**2)
+    tauP = 2*pi/(epsI * omega0)
+    psi0 = pi/2
+
+    psi = psi0 + 2*pi * (-t/tauP + sin(chi)**2 * t**2 / (2*tauS * tauP))
+
+    C = 1 - (cos(theta)*cos(chi))**2 - 0.5*(sin(theta)*sin(chi))**2
+
+    f = sin(2*theta)*sin(2*chi)*sin(psi) - (sin(theta)*sin(chi))**2*cos(2*psi)
+
+    g = (sin(2*theta)*sin(2*chi)*sin(2*pi*t/tauP - psi0) - 
+         (sin(theta)*sin(chi))**2*cos(4*pi*t/tauP + 2*psi0))
+
+    T0 = omega0 / tauS
+    T1 = -C
+    T2 =  3*C**2 * t/tauS
+    T3 = 0.5*f
+    T4 = -1.5*C*f*t/tauS
+    T5 = -(.75/pi)*g*C*tauP/tauS
+    T6 = 3/(pi*8.)* g*f*tauP/tauS
+
+    theta = a0
+    psidot = -epsI*omega0
+    EM = T0 * (T1 + T2 + T3 + T4 + T5 + T6)
+    GEOMETRIC = psidot**2 * (theta * cos(chi)/sin(chi) * cos(psi) + 
+                             .5*theta**2*(3+cos(2*chi))/(sin(chi))* sin(2*psi)
+                             )
+
+    return  (EM +  geometric * GEOMETRIC)/(2*pi)

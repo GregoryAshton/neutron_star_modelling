225d04566440c54e93d80a74179c631c5f384a82
Gregory Ashton <gash789@gmail.com>
2016-04-06 17:35:39 +0100

    Update P-Pdot diagram

--- Illustrations/Period_PeriodDot_plot/make_plot.py
+++ Illustrations/Period_PeriodDot_plot/make_plot.py
@@ -29,40 +29,40 @@ fig = plt.figure(figsize=(6,8))
 ax = fig.add_subplot(111)
 
 # Normal pulsars
-normal_df = df[(df.p0 > 1e-1) & (df.p1 > 0) & 
+normal_df = df[(df.p0 > 1e-1) & (df.p1 > 0) &
                (df.Binary == "*") & (df.Type=="*")
                & (df.Age > 1e5)]
 ax.scatter(normal_df.p0.values, normal_df.p1.values,
-           s = 10, c='k', marker="o", label="Normal pulsars")
+           s=2, c='k', marker="o", label="Normal pulsars")
 
 # Young pulsars
-normal_df = df[(df.p0 > 1e-1) & (df.p1 > 0) & 
+normal_df = df[(df.p0 > 1e-1) & (df.p1 > 0) &
                (df.Binary == "*") & (df.Type=="*")
                & (df.Age < 1e5)]
 ax.scatter(normal_df.p0.values, normal_df.p1.values,
-           s = 10, c='cyan', marker="o", label="Young pulsars")
+           s=22, c='cyan', marker="o", label="Young pulsars")
 
+# Binary normal pulars
+binary_normal_df = df[(df.p0 > 1e-1) & (df.p1 > 0) &
+               (df.Binary != "*") & (df.Type=="*")]
+ax.scatter(binary_normal_df.p0.values, binary_normal_df.p1.values,
+           s = 22, facecolors='r', edgecolors="k",
+           marker="s", label="Binary pulsars")
 
 # Millisecond pulsars
 MSP_df = df[(df.p0 < 1e-1) & (df.p1 < 1e-16) &
             (df.Binary == "*") & (df.Type=="*")]
 ax.scatter(MSP_df.p0.values, MSP_df.p1.values,
-           s = 20, c="k", facecolors="none", edgecolors="k", 
+           s = 22, facecolors="w", edgecolors="k",
            marker="o", label="MSP")
 
 # Binary MSP pulars
 binary_MSP_df = df[(df.p0 < 1e-1) & (df.p1 < 1e-16) &
             (df.Binary != "*") & (df.Type=="*")]
 ax.scatter(binary_MSP_df.p0.values, binary_MSP_df.p1.values,
-           s = 20, facecolors='r', edgecolors="k",
+           s = 22, facecolors='g', edgecolors="k",
            marker="o", label="Binary MSP")
 
-# Binary normal pulars
-binary_normal_df = df[(df.p0 > 1e-1) & (df.p1 > 0) & 
-               (df.Binary != "*") & (df.Type=="*")]
-ax.scatter(binary_normal_df.p0.values, binary_normal_df.p1.values,
-           s = 20, facecolors='b', edgecolors="k",
-           marker="o", label="Binary pulsars")
 
 # AXP
 magnetar_df = df[["AXP" in t for t in df.Type]]
@@ -73,39 +73,40 @@ P_range = np.logspace(-4, 4, 100)
 ## Add lines of constant magnetic field
 def Pdot(P, B0):
     CONST = 3.2e19
-    return (B0 / CONST) ** 2 / P  
-B0s = np.logspace(10, 16, 7)
+    return (B0 / CONST) ** 2 / P
+B0s = np.logspace(8, 16, 9)
 text_X = 1.5e1
 for B0 in B0s:
     power = np.log10(B0)
     if power != int(power):
         print "pow = {} is not an int".format(power)
-    ax.text(text_X, 0.8*Pdot(text_X, B0), 
-            "$B_{{0}}=$" + "$10^{{{}}}$".format(int(power)),
-            rotation=-38.,            
-            bbox=dict(facecolor='w', alpha=1.0, edgecolor="w", pad=0)
-            )
-    ax.plot(P_range, Pdot(P_range, B0), ls="--", 
-            lw=0.5, color="k")
+    if power > 9:
+        ax.text(text_X, 0.8*Pdot(text_X, B0),
+                "$B_{{0}}=$" + "$10^{{{}}}$ G".format(int(power)),
+                rotation=-38., size=10,
+                bbox=dict(facecolor='w', alpha=1.0, edgecolor="w", pad=0)
+                )
+    ax.plot(P_range, Pdot(P_range, B0), ls="--",
+            lw=0.5, color="k", zorder=-10)
 
 # Add lines of constant characteristic age
 def Pdot(P, tau):
-    return 0.5 * P/tau 
+    return 0.5 * P/tau
 tau_years = np.logspace(1, 9, 5)
 text_X = 2e-4
 for tau_year in tau_years:
     tau = tau_year * (365. * 86400)
-    rotation=33
+    rotation=35
     power = np.log10(tau_year)
     if power != int(power):
         print "pow = {} is not an int".format(power)
-    ax.text(text_X, 2.9*Pdot(text_X, tau), 
-            r"$\tau=$" + "$10^{{{}}}$".format(int(power)),
-            rotation=rotation,
+    ax.text(text_X, 3.9*Pdot(text_X, tau),
+            r"$\tau_c=$" + "$10^{{{}}}$ yrs".format(int(power)),
+            rotation=rotation, size=10,
             bbox=dict(facecolor='w', alpha=1.0, edgecolor="w", pad=0)
             )
-    ax.plot(P_range, Pdot(P_range, tau), ls="--", 
-            color="k", lw=0.5)
+    ax.plot(P_range, Pdot(P_range, tau), ls="--",
+            color="k", lw=0.5, zorder=-10)
 
 # Add the DEATH LINE
 #CONST = 1e-16
@@ -113,16 +114,15 @@ for tau_year in tau_years:
 
 ax.legend(loc=2, frameon=True, fontsize=14, numpoints=1)
 ax.set_xlabel("Period [s]")
-ax.set_ylabel("Period derivative")
+ax.set_ylabel("Period derivative [s/s]")
 
 
-xlims = (1e-4, 300)
+xlims = (1e-4, 150)
 ax.set_xlim(*xlims)
-ax.set_ylim(1e-22, 1e-7)
+ax.set_ylim(8e-22, 1e-8)
 
 ax.set_xscale('log')
 ax.set_yscale('log')
 
 fig.tight_layout()
 plt.savefig("Period_PeriodDot.pdf")
-plt.show()
